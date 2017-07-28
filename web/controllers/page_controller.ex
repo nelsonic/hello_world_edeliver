5f96f9425e54b4d0afbe429499c4230b2e50fbf6
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-28 11:36:31 +0100

    adds /_version endpoint to app which returns git revision id see: https://github.com/dwyl/learn-elixir/issues/53

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule HelloWorldEdeliver.PageController do
   use HelloWorldEdeliver.Web, :controller
 
   def index(conn, _params) do
+    {rev, i} = System.cmd("git", ["rev-parse", "HEAD"])
+    IO.puts(String.replace(rev, "\n", ""))
     render conn, "index.html"
+  end
+
+  def git_revision_hash(conn, _params) do
+    {rev, i} = System.cmd("git", ["rev-parse", "HEAD"])
+    text conn, String.replace(rev, "\n", "")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule HelloWorldEdeliver.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/_version", PageController, :git_revision_hash
   end
 
   # Other scopes may use custom stacks.
