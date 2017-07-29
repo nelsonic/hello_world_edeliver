03ef4e27150ae9380d3afecfdb7b70a30062645d
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-29 01:45:07 +0100

    add command to print working directory "pwd" in logs

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,13 +2,14 @@ defmodule HelloWorldEdeliver.PageController do
   use HelloWorldEdeliver.Web, :controller
 
   def index(conn, _params) do
-    {rev, i} = System.cmd("git", ["rev-parse", "HEAD"])
+    IO.inspect(System.cmd("pwd", []))
+    {rev, _} = System.cmd("git", ["rev-parse", "HEAD"])
     IO.puts(String.replace(rev, "\n", ""))
     render conn, "index.html"
   end
 
   def git_revision_hash(conn, _params) do
-    {rev, i} = System.cmd("git", ["rev-parse", "HEAD"])
+    {rev, _} = System.cmd("git", ["rev-parse", "HEAD"])
     text conn, String.replace(rev, "\n", "")
   end
 end
