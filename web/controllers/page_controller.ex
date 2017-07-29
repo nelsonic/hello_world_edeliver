f012e9696a8e69c2fcf6c81e19f65a3ecdecdd2c
nelsonic <contact.nelsonic+github@gmail.com>
2017-07-29 06:28:18 +0100

    reduce lines in git revision handler

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,19 +6,19 @@ defmodule HelloWorldEdeliver.PageController do
   end
 
   def git_revision_hash(conn, _params) do
-    IO.inspect(System.cmd("pwd", []))
-    {ls, _} = IO.inspect(System.cmd("ls", ["-a"]))
+    # IO.inspect(System.cmd("pwd", []))
+    # {ls, _} = IO.inspect(System.cmd("ls", ["-a"]))
+    #
+    # ls = String.split(ls, "\n")
+    # IO.inspect ls
 
-    ls = String.split(ls, "\n")
-    IO.inspect ls
-
-    unless Enum.member?(ls, ".git") do
+    unless Enum.member?(String.split(System.cmd("ls", ["-a"]), "\n"), ".git") do
       File.cd("./builds")
       IO.inspect(System.cmd("pwd", []))
     end
 
     {rev, _} = System.cmd("git", ["rev-parse", "HEAD"])
-    IO.puts(String.replace(rev, "\n", ""))
+    # IO.puts(String.replace(rev, "\n", ""))
     text conn, String.replace(rev, "\n", "")
   end
 end
