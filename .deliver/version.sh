77b283372516a8e641cc99015512e5e853301380
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-03 10:04:55 +0100

    adds *even moar* clarifying comments to .edeliver/version.sh script so its obvious what it does and why!

--- .deliver/version.sh
+++ .deliver/version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This Bash Script has one job: Return the Latest version of the app to be deployed!
 # get the current directory (.deliver) see: https://stackoverflow.com/questions/59895/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # list files by creation time (most recent first) and return the first one (latest release)
@@ -11,4 +12,8 @@ NO_APP_NAME=$(echo $FILENAME | sed 's/hello_world_edeliver_//g') # e.g: 0.0.3+f0
 VERSION=$(echo $NO_APP_NAME | sed 's/.upgrade.tar.gz//g') # e.g: 0.0.3+f012e96
 
 # return the upgrade version for use in "mix edeliver deploy upgrade" command:
-echo $VERSION # e.g: 0.0.3+f012e96
+echo $VERSION # e.g: 0.0.3+f012e96 which is the version in mix.exs + short git hash: f012e96
+
+# The reason for using the Git Hash in the version of the app being deployed e.g: f012e96
+# so that we can search for it on GitHub and know which version of the code we are running
+# e.g: https://github.com/nelsonic/hello_world_edeliver/search?q=f012e96&type=Commits
