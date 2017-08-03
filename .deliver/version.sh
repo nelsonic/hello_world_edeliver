4807634f32d17b5d01f3ab19e3010f0c7e5b14dc
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-03 10:07:19 +0100

    move all descriptive comments to top of file in version.sh

--- .deliver/version.sh
+++ .deliver/version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# This Bash Script has one job: Return the Latest version of the app to be deployed!
+
+# This Bash Script has/does One Job: Return the Latest Version of the app to be deployed.
+# The reason for using the Git Hash in the version of the app being deployed e.g: f012e96
+# so that we can search for it on GitHub and know which version of the code we are running
+# e.g: https://github.com/nelsonic/hello_world_edeliver/search?q=f012e96&type=Commits
+
 # get the current directory (.deliver) see: https://stackoverflow.com/questions/59895/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # list files by creation time (most recent first) and return the first one (latest release)
@@ -13,7 +18,3 @@ VERSION=$(echo $NO_APP_NAME | sed 's/.upgrade.tar.gz//g') # e.g: 0.0.3+f012e96
 
 # return the upgrade version for use in "mix edeliver deploy upgrade" command:
 echo $VERSION # e.g: 0.0.3+f012e96 which is the version in mix.exs + short git hash: f012e96
-
-# The reason for using the Git Hash in the version of the app being deployed e.g: f012e96
-# so that we can search for it on GitHub and know which version of the code we are running
-# e.g: https://github.com/nelsonic/hello_world_edeliver/search?q=f012e96&type=Commits
