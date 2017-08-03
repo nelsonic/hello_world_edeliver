a80e0b170b89de2248606ad538684f5915062550
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-03 11:33:22 +0100

    adds test for /_version endpoint :tada:

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,21 +4,4 @@ defmodule HelloWorldEdeliver.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-
-  def git_revision_hash(conn, _params) do
-    IO.inspect(System.cmd("pwd", []))
-    {ls, _} = IO.inspect(System.cmd("ls", ["-a"]))
-    IO.inspect ls
-    ls = String.split(ls, "\n")
-    IO.inspect ls
-
-    unless Enum.member?(ls, ".git") do
-      File.cd("./builds")
-      IO.inspect(System.cmd("pwd", []))
-    end
-
-    {rev, _} = System.cmd("git", ["rev-parse", "HEAD"])
-    IO.puts(String.replace(rev, "\n", ""))
-    text conn, String.replace(rev, "\n", "")
-  end
 end
